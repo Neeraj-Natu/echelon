9577b49094b28acd2306a55ff0dca79aa1a8e95e
stanajin <natu.neeraj@gmail.com>
2020-03-08 23:09:00 +0530

    lexer start

---
+++ src/lexer/lexer_test.go
@@ -0,0 +1,44 @@
+package lexer
+
+import (
+	"testing"
+	"token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){}[],;:`
+
+	tests := []struct {
+		expectedType token.TokenType
+		expectedLiteral string
+	} {
+		{token.ASSIGN, "="},
+		{token.PLUS = "+"},
+		{token.LPAREN = "("},
+		{token.RPAREN = ")"},
+		{token.LCBRACE = "{"},
+		{token.RCBRACE = "}"},
+		{token.LBRACE = "["},
+		{token.RBRACE = "]"},
+		{token.COMMA = ","},
+		{token.SEMICOLON = ";"},
+		{token.COLON = ":"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
---
+++ src/token/token.go
@@ -0,0 +1,70 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Indentifiers and literals
+	VARIABLE = "VAR" // add, foobar, x, y, ......
+	INT = "INT" // 123424
+	FLOAT = "FLOAT" // 1231234.31251512
+
+	// Operators
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	AND = "&"
+	OR = "||"
+	
+	EQ = "=="
+	NOT_EQ = "!="
+	LT = "<"
+	GT = ">"
+
+	// Delimiters
+	COMMA = ","
+	SEMICOLON = ";"
+	COLON = ":"
+	LPAREN = "("
+	RPAREN = ")"
+	LCBRACE = "{"
+	RCBRACE = "}"
+	LBRACE = "["
+	RBRACE = "]"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+	LET = "LET"
+)
+
+var keywords = map[string]TokenType {
+	"func": FUNCTION,
+	"let": LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string TokenType) {
+	if tok, ok := keywords[ident]; ok {
+		retun tok
+	}
+	retun VARIABLE
+}
