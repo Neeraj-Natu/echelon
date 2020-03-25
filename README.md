433c47426e87d7f08549b877220b6fc8dbb27a41
stanajin <natu.neeraj@gmail.com>
2020-03-25 16:57:18 +0530

    adding readMe

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # shifu
 
+![Shifu](./imgs/shifu.jpg "Shifu")
+
 This is a personal project intended to write my own interpreter in go lang.
 
 Langage Featuers:
+
 - Dynamically typed
 - Supports most of the control statements
 - Supports strings and integers
+- Functions are first class citizens
+- Completely written in golang
--- ast/ast.go
+++ ast/ast.go
@@ -7,7 +7,7 @@ import (
 // The base Node interface
 type Node interface {
 	TokenLiteral() string
-	String() string
+	// String() string
 }
 
 // All statement nodes implement this
@@ -21,3 +21,34 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Any let statement has 3 parts first the token LET, second the variable name and third the expression that it points to on the right side of equals sign.
+type LetStatement struct {
+	Token token.Token  // the token.LET token
+	Name *Variable
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) TokenLiteral() string {return ls.Token.Literal}
+
+// This is to hold the variable in the let statement. This implements the expression interface.
+type Variable struct {
+	Token token.Token // the token.VARIABLE token
+	Value string
+}
+
+func (i *Variable) expressionNode() {}
+func (i *Variable) TokenLiteral() string {return i.Token.Literal}
