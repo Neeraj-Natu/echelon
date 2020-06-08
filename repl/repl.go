3bb743a973d92ddfc5b7009a87b14d3543d04b41
stanajin <natu.neeraj@gmail.com>
2020-06-09 00:58:16 +0530

    adding in parser to repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,15 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+
 	"github.com/Neeraj-Natu/shifu/lexer"
-	"github.com/Neeraj-Natu/shifu/token"
+	"github.com/Neeraj-Natu/shifu/parser"
 )
 
 /*
-read from the input source until encountering a newline, 
-take the just read line and pass it to an instance of our 
-lexer and finally print all the tokens the lexer gives us 
-until we encounter EOF.
+read from the input source until encountering a newline,
+take the just read line and pass it to an instance of our
+lexer and that to our Parser once the parser is done with
+it's job we print out the errors or the Parsed program by
+calling String() method on Program that recursively calls
+the String() method on all of the statements belonging to
+that program.
 */
 
 const PROMPT = ">> "
@@ -29,9 +33,33 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
 }
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, SHIFU)
+	io.WriteString(out, "Learning code is an art that takes years to master. Do not be disappointed if you have failed")
+	io.WriteString(out, "parser errors: \n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
+}
+
+const SHIFU = `		__,__
+   ____           __________     ________     
+ /       |     |      |         |            |        |
+|        |     |      |         |            |        |
+ \____   |_____|      |         |________    |        |
+	  \  |     |      |         |            |        |
+       | |     |      |         |            |        |
+______/  |     |  ____|_____    |             \______/
+`
