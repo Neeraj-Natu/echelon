9a2e946d0f76594fcdbfac346567dcfc2ed8b786
stanajin <natu.neeraj@gmail.com>
2020-06-10 00:08:16 +0530

    support for ifelse evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -31,6 +31,10 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 	return nil
 }
@@ -44,6 +48,7 @@ func evalStatements(stmnts []ast.Statement) object.Object {
 	return result
 }
 
+// This function's purpose is to convert the native bool to our Boolean Object
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
@@ -51,6 +56,7 @@ func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	return FALSE
 }
 
+//There are just two different prefixes supported in Shifu language helper for them follow this function.
 func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
@@ -84,6 +90,10 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	return &object.Integer{Value: -value}
 }
 
+//This function evaluates the infix Expressions it uses pointer comparision for most checks apart from Integer
+//As while evaluating Integers we assign new Objects and hence new memory addresses to them
+//Comparing different memory addresses won't work thus they need their own helper function to compare the
+//values extracted from them.
 func evalInfixExpression(operator string, left, right object.Object) object.Object {
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
@@ -124,5 +134,33 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+//Here we decided the if condition should evaluate to TRUE anytime when the condition is not false or null.
+//Instead of being explicity TRUE. Also incase the condition doesn't evaluate to a value it's supposed to return NULL.
+//These are language design decisions governed by 'isTruthy' function.
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return TRUE
 	}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -63,6 +63,7 @@ func TestEvalBooleanExpression(t *testing.T) {
 		{"true && true", true},
 		{"true || false", true},
 		{"true && false", false},
+		{"false && false", false},
 		{"false || false", false},
 		{"(1 + 3 == 4) || (2 * 5 < 6)", true},
 		{"((23 + 46 * 7) > 8) && ((43 - 435) > 324)", false},
@@ -90,6 +91,31 @@ func TestBangOperator(t *testing.T) {
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
 		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestIfElseExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{"if(true){10}", 10},
+		{"if(false){10}", nil},
+		{"if(1){10}", 10},
+		{"if(1 < 2){10}", 10},
+		{"if(1 > 2){10}", nil},
+		{"if(1 > 2 ){10} else {20}", 20},
+		{"if(1 < 2){10} else {20}", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer))
+		} else {
+			testNullObject(t, evaluated)
+		}
 	}
 }
 
@@ -124,6 +150,14 @@ func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
 
 	if result.Value != expected {
 		t.Errorf("object has wrong value. got=%t, expected=%t", result.Value, expected)
+		return false
+	}
+	return true
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+	if obj != NULL {
+		t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
 		return false
 	}
 	return true
